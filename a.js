9ba287ddec1c2bfc99d54cd63b0de08bad0e1ac3
Cristian Ramirez <ccrhdbj@outlook.com>
2022-08-03 13:45:10 -0500

    Update js

--- a.js
+++ a.js
@@ -1,12 +1,9 @@
 setInterval(function obtHora() 
 {
-  var hora = Date();
-  let re = /[0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2}/;
-  output = hora.match(re);
-  var division = output[0].split(":");
-  document.getElementById("horas").innerHTML = division[0];
-  document.getElementById("minutos").innerHTML = division[1];
-  document.getElementById("segundos").innerHTML = division[2];
+  var hora = new Date();
+  document.getElementById("horas").innerHTML = hora.getHours();
+  document.getElementById("minutos").innerHTML = hora.getMinutes();
+  document.getElementById("segundos").innerHTML = hora.getSeconds();
 }, 
 1000,);
 
