4461a2ca01075858777935827847d6e1584da4fc
Cristian Ramirez <ccrhdbj@outlook.com>
2022-09-20 10:26:45 -0500

    Convert Resposive Design

--- a.js
+++ a.js
@@ -1,4 +1,4 @@
-setInterval(function obtHora() 
+/* setInterval(function obtHora() 
 {
   var hora = new Date();
   document.getElementById("horas").innerHTML = hora.getHours();
@@ -6,4 +6,13 @@ setInterval(function obtHora()
   document.getElementById("segundos").innerHTML = hora.getSeconds();
 }, 
 1000,);
-
+ */
+setInterval(function obtHora() {
+  var hora = Date();
+  let re = /[0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2}/;
+  output = hora.match(re);
+  var division = output[0].split(":");
+  document.getElementById("horas").innerHTML = division[0];
+  document.getElementById("minutos").innerHTML = division[1];
+  document.getElementById("segundos").innerHTML = division[2];
+}, 1000);
