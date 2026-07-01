a7e586864b6cc55e558057c43693900e1be588fc
Cristian Ramirez <ccrhdbj@outlook.com>
2026-07-01 20:56:09 +0000

    Agregar soporte para PWA: incluir manifest.json, service worker y nuevos íconos

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'futureclock-v1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './style.css',
+  './a.js',
+  './manifest.json',
+  './icons/icon.svg',
+  './icons/icon-192.svg',
+  './icons/icon-512.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((networkResponse) => {
+        const responseClone = networkResponse.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseClone));
+        return networkResponse;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match('./index.html')))
+  );
+});
